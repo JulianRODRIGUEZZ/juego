84b594389d5aeb0519a1a15c4149f6f21d43de63
JulianRODRIGUEZZ <116646357+JulianRODRIGUEZZ@users.noreply.github.com>
2023-05-15 00:11:34 -0500

    Add files via upload

---
+++ juego-main/Juego/carga.js
@@ -0,0 +1,3 @@
+setTimeout(function(){
+    window.location.href = "file:///C:/Users/lenovo/Desktop/juego-main/Juego/index.html"; //Cuando el usuario haga clic en el botón, se iniciará un temporizador y luego se redirigirá automáticamente a la nueva página.
+  }, 5000); // 
